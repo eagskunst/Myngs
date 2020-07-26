86a91c39b81309f3911fcf6a673773c3fe1293c5
Emmanuel Guerra <emmanuelnypd@gmail.com>
2020-07-25 23:27:15 -0400

    BindingAdapter for loading images url strings.  Song placeholder drawable

---
+++ base-android/src/main/java/com/eagskunst/apps/myngs/base_android/databinding/Bindings.kt
@@ -0,0 +1,28 @@
+package com.eagskunst.apps.myngs.base_android.databinding
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.api.load
+import com.eagskunst.apps.myngs.base_android.R
+
+/**
+ * Created by eagskunst in 25/7/2020.
+ */
+
+@BindingAdapter("loadImageUrl")
+fun loadImageUrl(imageView: ImageView, url: String?) {
+    url?.let {
+        imageView.load(it) {
+            crossfade(true)
+        }
+    }
+}
+
+fun loadSongImage(imageView: ImageView, url: String?) {
+    url?.let {
+        imageView.load(it) {
+            crossfade(true)
+            placeholder(R.drawable.song_placeholder)
+        }
+    }
+}
