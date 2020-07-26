5ea76e7f185eefdf7cdc5eea5e114467c37f92dd
Emmanuel Guerra <emmanuelnypd@gmail.com>
2020-07-25 23:29:16 -0400

    Epoxy data binding config file

---
+++ app/src/main/java/com/eagskunst/apps/myngs/EpoxyDataBindingConfig.java
@@ -0,0 +1,9 @@
+package com.eagskunst.apps.myngs;
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern;
+
+/**
+ * Created by eagskunst in 25/7/2020.
+ */
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "view_holder")
+interface EpoxyDataBindingConfig { }
